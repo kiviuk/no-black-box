38009dccac6dbeda31eb76d414af9837b095ece2
Mobe <mo@be.org>
2023-10-29 03:49:32 +0100

    - common/featureFunctions.js: Added a check to see if the 'geometry' module is defined, and if not, require it. - common/featureFunctions.js: Added a new function 'getElongation' to calculate the elongation of a shape. - common/featureFunctions.js: Added the new function 'getElongation' to the 'inUse' array. - common/featureFunctions.js: Changed the formatting of the 'inUse' array to include the new 'Elongation' function.

--- common/featureFunctions.js
+++ common/featureFunctions.js
@@ -1,3 +1,7 @@
+if (typeof geometry === "undefined") {
+    geometry = require("./geometry.js");
+}
+
 const featureFunctions = {};
 
 featureFunctions.getPathCount = (paths) => {
@@ -25,9 +29,17 @@ featureFunctions.getHeight = (paths) => {
     return max - min;
 }
 
+featureFunctions.getElongation = (paths) => {
+    const points = paths.flat();
+    const {width, height} = geometry.minimumBoundingBox({points});
+    return (Math.max(width, height) + 1) /
+        (Math.min(width, height) + 1);
+};
+
 featureFunctions.inUse = [
     {name: "Width", function: featureFunctions.getWidth},
-    {name: "Height", function: featureFunctions.getHeight}
+    {name: "Height", function: featureFunctions.getHeight},
+    {name: "Elongation", function: featureFunctions.getElongation},
 ];
 
 
